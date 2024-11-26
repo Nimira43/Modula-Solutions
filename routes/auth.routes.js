8fc8d354d9e2df7252f81c7951253dc69d7ab3a5
Nick Rabone <n.rabone72@aol.com>
2024-11-26 08:33:33 +0000

    authController.getSignup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,6 @@ const express = require('express')
 const authController = require('../controllers/auth.controller')
 const router = express.Router()
 
-router.get('/signup', )
+router.get('/signup', authControll.getSignup )
 
 module.exports = router
