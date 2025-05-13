fcb27d28c9441d1af4ee2570e0b9a84d320047de
Nimira43 <n.rabone72@aol.com>
2025-05-13 20:56:08 +0100

    Updated HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function HomePage() {
-  return (
+const HomePage = () => {
+  return ( 
     <div className='p-5 bg-[#ff4500]'>
       <h1 className='logo text-[#fffcfa] text-3xl'>Modulas</h1>
     </div>
   )
 }
+ 
+export default HomePage
