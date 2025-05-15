6ec5300894869a100520aa015797cffe0764753a
Nimira43 <n.rabone72@aol.com>
2025-05-15 23:01:38 +0100

    Brought in Theme colours from the Amphora project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import { HiOutlineTicket } from 'react-icons/hi2'
 const HomePage = () => {
   return ( 
     <main className='flex flex-col text-center items-center justify-center min-h-screen px-4'>
-      <h1 className='text-3xl md:text:4xl font-medium mb-4 text-orange-600'>Modulas Solutions</h1>
+      <h1 className='text-3xl md:text:4xl font-medium mb-4 text-prime'>Modulas Solutions</h1>
+      <p className='text-lg text-'>Support Ticket Management System</p>
+
+      <div className='flex flex-col'></div>
     </main>
   )
 }
