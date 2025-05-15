9467beb90ae211c32a5b983d4bf6f7d8d6c514ab
Nimira43 <n.rabone72@aol.com>
2025-05-15 22:45:47 +0100

    Working on HomePage - parent div and h1. Brought in imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+import { HiOutlineTicket } from 'react-icons/hi2'
+
 const HomePage = () => {
   return ( 
-    <div className='p-5 bg-[#ff4500]'>
-      <h1 className='logo text-[#fffcfa] text-3xl'>Modulas</h1>
-    </div>
+    <main className='flex flex-col text-center items-center justify-center min-h-screen px-4'>
+      <h1 className='text-3xl md:text:4xl font-medium mb-4 text-orange-600'>Modulas Solutions</h1>
+    </main>
   )
 }
  
